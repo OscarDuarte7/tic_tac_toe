31bac2b555767224923076842cb6608a05dad291
unknown <o.duarte7@gmail.com>
2019-04-21 19:43:03 -0500

    Primer Commit

---
+++ Assets/Hand.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        tag = "P1";
+    }
+
+    void Update()
+    {
+        move();
+    }
+
+
+    void move()
+    {
+        float moveHorizontal = 0.0f;
+        float moveForward = 0.0f;
+        float moveVertical = 0.0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveForward = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveForward = -1.0f;
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            moveHorizontal = -1.0f;
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveHorizontal = 1.0f;
+
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveVertical = 1.0f;
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveVertical = -1.0f;
+
+        }
+
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, moveForward);
+        rb.velocity = movement * speed;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(Input.GetKey(KeyCode.F) && collision.gameObject.tag == "W")
+        {
+
+            if(tag == "P1")
+            {
+                collision.gameObject.tag = "X";
+                collision.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+                tag = "P2";
+                transform.position = new Vector3(0, 0, 0);
+
+            }
+            else
+            {
+                collision.gameObject.tag = "O";
+                collision.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
+                tag = "P1";
+                transform.position = new Vector3(0, 0, 0);
+
+
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        GetComponent<MeshRenderer>().material.SetColor("_Color", Color.white);
+
+    }
+
+    // Update is called once per frame
+    /* void Update()
+     {
+
+         if (Input.GetKey(KeyCode.W))
+         {
+             transform.position += new Vector3(0, 0, 0.5f);
+
+         }
+         else if (Input.GetKey(KeyCode.S))
+         {
+             transform.position += new Vector3(0, 0, -0.5f);
+
+         }
+         else if (Input.GetKey(KeyCode.A))
+         {
+             transform.position += new Vector3(-0.5f, 0, 0);
+
+         }
+         else if (Input.GetKey(KeyCode.D))
+         {
+             transform.position += new Vector3(0.5f, 0, 0);
+
+         }
+         else if (Input.GetKey(KeyCode.UpArrow))
+         {
+             transform.position += new Vector3(0, 0.5f, 0);
+
+         }
+         else if (Input.GetKey(KeyCode.DownArrow))
+         {
+             transform.position += new Vector3(0, -0.5f, 0);
+
+         }
+
+     }*/
+}
---
+++ Assets/World.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+
+    private GameObject[,] tablero;
+    int largoFila = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int distZ = 10;
+        tablero = new GameObject[largoFila, largoFila];
+
+        for ( int i = 0; i < largoFila; i++)
+        {
+
+            for (int j = 0; j < largoFila; j++)
+            {
+                tablero[i, j] = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                tablero[i, j].transform.position = new Vector3(((largoFila * i)- largoFila), largoFila - j*largoFila, distZ);
+                tablero[i, j].tag = "W";
+                tablero[i, j].transform.localScale *= 2;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool wP1 = false;
+        bool wP2 = false;
+
+        int cP1 = 0;
+        int cP2 = 0;
+        //check diagonal i-d
+        for (int i = 0; i < largoFila; i++)
+        {
+  
+
+     
+
+                if (tablero[i, i].tag == "X")
+                {
+                    cP1++;
+                }
+                else if (tablero[i, i].tag == "O")
+                {
+                    cP2++;
+                }
+
+            
+
+            if (cP1 == 3 && cP2 != 3)
+            {
+                wP1 = true;
+
+            }
+            else if (cP2 == 3 && cP1 != 3)
+            {
+                wP2 = true;
+
+            }
+            else if (cP2 == 3 && cP1 == 3)
+            {
+                wP1 = false;
+                wP2 = false;
+
+            }
+
+
+
+        }
+
+        //check diagonal d-i
+
+        cP1 = 0;
+        cP2 = 0;
+        for (int i = 0; i < largoFila; i++)
+        {
+          
+
+            for (int j = 0; j < largoFila; j++)
+            {
+                if ((i + j) % largoFila == 2)
+                {
+
+                    if (tablero[i, j].tag == "X")
+                    {
+                        cP1++;
+                    }
+                    else if (tablero[i, j].tag == "O")
+                    {
+                        cP2++;
+                    }
+                }
+            }
+
+            if (cP1 == 3 && cP2 != 3)
+            {
+                wP1 = true;
+
+            }
+            else if (cP2 == 3 && cP1 != 3)
+            {
+                wP2 = true;
+
+            }
+            else if (cP2 == 3 && cP1 == 3)
+            {
+                wP1 = false;
+                wP2 = false;
+
+            }
+
+
+
+        }
+
+        //check columnas
+        for (int i = 0; i < largoFila; i++)
+        {
+             cP1 = 0;
+             cP2 = 0;
+
+            for (int j = 0; j < largoFila; j++){
+
+                if (tablero[i, j].tag == "X")
+                {
+                    cP1++;
+                }
+                else if (tablero[i, j].tag == "O")
+                {
+                    cP2++;
+                }
+
+            }
+
+            if (cP1 == 3 && cP2 != 3)
+            {
+                wP1 = true;
+               
+            }
+            else if(cP2 == 3 && cP1 != 3)
+            {
+                wP2 = true;
+
+            }
+            else if (cP2 == 3 && cP1 == 3)
+            {
+                wP1 = false;
+                wP2 = false;
+
+            }
+
+
+
+        }
+
+
+        //check filas
+        for (int i = 0; i < largoFila; i++)
+        {
+             cP1 = 0;
+             cP2 = 0;
+
+            for (int j = 0; j < largoFila; j++)
+            {
+
+                if (tablero[j,i].tag == "X")
+                {
+                    cP1++;
+                }
+                else if (tablero[j, i].tag == "O")
+                {
+                    cP2++;
+                }
+
+            }
+
+            if (cP1 == 3 && cP2 != 3)
+            {
+                wP1 = true;
+
+            }
+            else if (cP2 == 3 && cP1 != 3)
+            {
+                wP2 = true;
+
+            }
+            else if (cP2 == 3 && cP1 == 3)
+            {
+                wP1 = false;
+                wP2 = false;
+
+            }
+
+
+
+        }
+
+        if (wP1)
+        {
+            Debug.Log("P1 ganó!");
+            Application.LoadLevel(Application.loadedLevel);
+
+        }
+        else if (wP2)
+        {
+            Debug.Log("P2 ganó!");
+            Application.LoadLevel(Application.loadedLevel);
+
+
+        }
+        else
+        {
+            Debug.Log("Nadie ganó!");
+
+        }
+
+
+    }
+
+   
+}
